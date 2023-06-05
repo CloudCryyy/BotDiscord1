2b642602c7f5aa4c01fbd6e3c1754194b2ce94f5
CloudCryyy <cloud.cryy@gmail.com>
2023-06-05 12:09:50 -0300

    Adicionado mais algumas coisas no bot

--- commands/openbook.js
+++ commands/openbook.js
@@ -1,11 +1,19 @@
 module.exports = {
-    name:'openbook',
-    description:'retorna o link para algum livro útil para o jogo',
+  name: 'openbook',
+  description: 'retorna o link para algum livro útil para o jogo',
   async execute(message, args) {
-    if (args == "ldj"){
-      await message.channel.send('https://ordempendragon.files.wordpress.com/2017/04/dd-5e-livro-do-jogador-fundo-branco-biblioteca-c3a9lfica.pdf')
-    } else if (args == "mm"){
-      await message.channel.send('https://ordempendragon.files.wordpress.com/2017/04/monster-a-day-compendium-upgraded.pdf')
+    if (args == 'ldj') {
+      await message.channel.send(
+        'https://ordempendragon.files.wordpress.com/2017/04/dd-5e-livro-do-jogador-fundo-branco-biblioteca-c3a9lfica.pdf'
+      )
+    } else if (args == 'mm') {
+      await message.channel.send(
+        'https://ordempendragon.files.wordpress.com/2017/04/monster-a-day-compendium-upgraded.pdf'
+      )
+    } else if (args == 'sp') {
+      await message.channel.send(
+        'https://homebrewery.naturalcrit.com/share/r1mnLrkNrZ'
+      )
     }
   }
 }
