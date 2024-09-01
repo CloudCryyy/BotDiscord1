04c76c539991c057fa3138d7c3f51f8ec322eca7
CloudCryyy <cloud.cryy@gmail.com>
2024-09-01 15:51:04 -0300

    feat: LOUIE TÁ DE VOLTA PORRA

--- commands/rolar.js
+++ commands/rolar.js
@@ -1,25 +1,30 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export default {
-    async execute(interaction) {
-        const subCommand = interaction.option.getSubcommand();
-
-        if(subCommand === "dado"){
-            await interaction.reply("to tentando")
-        }
-
-    },
-
     data: new SlashCommandBuilder()
         .setName("rolar")
-        .setDescription("Rola um dado de alguns lados")
+        .setDescription("Rola uns dados")
         .addNumberOption((option) =>
             option
-                .setName("vezes")
-                .setDescription("decide quantas vezes o dado vai ser rodado")
+                .setName('lados')
+                .setDescription('escolhe o número de lados')
                 .setRequired(true))
-        .addNumberOption((option)=>
+        .addNumberOption((option) =>
             option
-                .setName("d20")
-                .setDescription("rola o dado de 20 lados"))
+                .setName('vezes')
+                .setDescription('escolhe o número de vezes')
+                .setRequired(true)),
+        async execute(interaction){
+            const lados = interaction.options.get('lados').value;
+            let vezes = interaction.options.get('vezes');
+            if (vezes.value < 1){
+                vezes.value = 1;
+            }
+            const dado = Math.floor(Math.random() * lados) + 1
+            await interaction.reply('Dado caiu em: ' + dado)
+            for (let i = 1; i < vezes.value; i++) {
+                const dado = Math.floor(Math.random() * lados) + 1
+                await interaction.followUp('Dado caiu em: ' + dado)
+            }
+        }
 }
