21cacf5f88a37baa33c69252825badb8cfe342be
CloudCryyy <cloud.cryy@gmail.com>
2024-09-01 11:33:59 -0300

    feat: tentando adicionar subcomandos

--- commands/rolar.js
+++ commands/rolar.js
@@ -1,10 +1,25 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export default {
+    async execute(interaction) {
+        const subCommand = interaction.option.getSubcommand();
+
+        if(subCommand === "dado"){
+            await interaction.reply("to tentando")
+        }
+
+    },
+
     data: new SlashCommandBuilder()
         .setName("rolar")
-        .setDescription("Rola algum dado"),
-    async execute(interaction) {
-        await interaction.reply("Rodei, muito obrigado")
-    }
+        .setDescription("Rola um dado de alguns lados")
+        .addNumberOption((option) =>
+            option
+                .setName("vezes")
+                .setDescription("decide quantas vezes o dado vai ser rodado")
+                .setRequired(true))
+        .addNumberOption((option)=>
+            option
+                .setName("d20")
+                .setDescription("rola o dado de 20 lados"))
 }
